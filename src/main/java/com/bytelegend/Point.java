692ea710a1ae28cca216547c7d218cfffa510f86
saveunhappy <925147954@qq.com>
2022-08-09 07:39:46 +0000

    Update at 2022-08-09T07:39:46.557980Z

--- src/main/java/com/bytelegend/Point.java
+++ src/main/java/com/bytelegend/Point.java
@@ -59,9 +59,9 @@ public class Point {
 
         Collections.sort(points, (p1, p2) -> {
             if (p1.x == p2.x) {
-                return p1.y - p2.y;
+                return Integer.compare(p1.y,p2.y);
             }
-            return p1.x - p2.x;
+            return Integer.compare(p1.x,p2.x);
         });
         return points;
     }
