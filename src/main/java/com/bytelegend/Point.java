ec12b1d3220cdeac1f4971a6cb4034903fdcd382
YoungerKayn <79000036+YoungerKayn@users.noreply.github.com>
2023-01-11 13:53:11 +0000

    Update at 2023-01-11T13:53:11.431659Z

--- src/main/java/com/bytelegend/Point.java
+++ src/main/java/com/bytelegend/Point.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable {
     private final int x;
     private final int y;
 
@@ -50,11 +50,18 @@ public class Point {
         return String.format("(%d,%d)", x, y);
     }
 
+    @Override
+    public int compareTo(Object o) {
+        Point p = (Point) o;
+        return this.equals(p) ? 0 : ((this.x > 0 && p.getX() < 0) ? 1 : (this.x < 0 && p.getX() > 0 ? -1 : (this.x == p.getX() ? ((this.y > 0 && p.getY() < 0) ? 1 : ((this.y < 0 && p.getY() > 0) ? -1 : this.y - p.getY())) : this.x - p.getX())));
+    }
+
     /**
      * Sort a list of `Point` in ascending order of `x` first and then `y`. For example, a sorted
      * list: `(-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)`. Hint: you can use `Collections.sort()` method.
      */
     public static List<Point> sort(List<Point> points) {
+        points.sort(null);
         return points;
     }
 
